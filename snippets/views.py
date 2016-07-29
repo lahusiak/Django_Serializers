a29512fa29d04b740d07a1b134fc3f2ffd2c7cae
lahusiak <laryssa.husiak@buzzfeed.com>
2016-07-29 14:36:02 -0500

    Fixed permissions error

--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,7 @@ from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
 
 
@@ -19,6 +20,8 @@ class UserDetail(generics.RetrieveAPIView):
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly, )
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -27,8 +30,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    #keeps erroring
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
+                          IsOwnerOrReadOnly, )
 
 
